bff5928d80f163aea290bfa0f0adb6503159071a
sbsimf <sbsimf@gmail.com>
2017-02-20 15:18:24 +0300

    commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://roza/admin/');
+define('HTTP_CATALOG', 'http://roza/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://roza/admin/');
+define('HTTPS_CATALOG', 'http://roza/');
+
+// DIR
+define('DIR_APPLICATION', 'S:/OpenServer/domains/roza/admin/');
+define('DIR_SYSTEM', 'S:/OpenServer/domains/roza/system/');
+define('DIR_IMAGE', 'S:/OpenServer/domains/roza/image/');
+define('DIR_LANGUAGE', 'S:/OpenServer/domains/roza/admin/language/');
+define('DIR_TEMPLATE', 'S:/OpenServer/domains/roza/admin/view/template/');
+define('DIR_CONFIG', 'S:/OpenServer/domains/roza/system/config/');
+define('DIR_CACHE', 'S:/OpenServer/domains/roza/system/storage/cache/');
+define('DIR_DOWNLOAD', 'S:/OpenServer/domains/roza/system/storage/download/');
+define('DIR_LOGS', 'S:/OpenServer/domains/roza/system/storage/logs/');
+define('DIR_MODIFICATION', 'S:/OpenServer/domains/roza/system/storage/modification/');
+define('DIR_UPLOAD', 'S:/OpenServer/domains/roza/system/storage/upload/');
+define('DIR_CATALOG', 'S:/OpenServer/domains/roza/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'roza');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://roza/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://roza/');
+
+// DIR
+define('DIR_APPLICATION', 'S:/OpenServer/domains/roza/catalog/');
+define('DIR_SYSTEM', 'S:/OpenServer/domains/roza/system/');
+define('DIR_IMAGE', 'S:/OpenServer/domains/roza/image/');
+define('DIR_LANGUAGE', 'S:/OpenServer/domains/roza/catalog/language/');
+define('DIR_TEMPLATE', 'S:/OpenServer/domains/roza/catalog/view/theme/');
+define('DIR_CONFIG', 'S:/OpenServer/domains/roza/system/config/');
+define('DIR_CACHE', 'S:/OpenServer/domains/roza/system/storage/cache/');
+define('DIR_DOWNLOAD', 'S:/OpenServer/domains/roza/system/storage/download/');
+define('DIR_LOGS', 'S:/OpenServer/domains/roza/system/storage/logs/');
+define('DIR_MODIFICATION', 'S:/OpenServer/domains/roza/system/storage/modification/');
+define('DIR_UPLOAD', 'S:/OpenServer/domains/roza/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'roza');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
